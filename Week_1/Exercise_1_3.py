87f98ba1f0643dc452d36c25970e0cf75f7274be
KGBeazt <alexkravtsov95@gmail.com>
2025-08-29 14:24:13 -0500

    complete exercise 1.3

--- Week_1/Exercise_1_3.py
+++ Week_1/Exercise_1_3.py
@@ -1,19 +1,12 @@
 # Exercise 1.3
-# Calculate discount for customer 1
-price1 = 100
-discount1 = 0.10
-final_price1 = price1 - (price1 * discount1)
+# Function:
+def Disc_Cal(p, d):
+    price = p 
+    discount = d 
+    final_price = price - (price * discount)
+    print(f"The discounted price of your item is: ${final_price}\n")
 
-# Calculate discount for customer 2
-price2 = 250
-discount2 = 0.15
-final_price2 = price2 - (price2 * discount2)
-
-# Calculate discount for customer 3
-price3 = 50
-discount3 = 0.05
-final_price3 = price3 - (price3 * discount3)
-
-print(f"Final price for customer 1: ${final_price1}")
-print(f"Final price for customer 2: ${final_price2}")
-print(f"Final price for customer 3: ${final_price3}")
+# Execution
+Disc_Cal(100, 0.10)
+Disc_Cal(250, 0.15)
+Disc_Cal(50, 0.05)
