64f55f36b99fc606c06a021ba103542c05f49633
KGBeazt <alexkravtsov95@gmail.com>
2025-08-29 14:18:12 -0500

    Complete exercise 1.1

--- Week_1/Exercise_1_1.py
+++ Week_1/Exercise_1_1.py
@@ -1,21 +1,19 @@
-# Code for exercise 1 - Work with Functions
-# Calculating area of rectangles
-length1 = 10
-width1 = 5
-area_rectangle1 = length1 * width1
+# Example 1
+#Functions:
+def Rectangle_Area(l, w): # Calculating area of rectangles
+    length = l 
+    width = w
+    area_rectangle1 = length * width
+    print("Area of the rectangle is:", area_rectangle1)
 
-length2 = 15
-width2 = 7
-area_rectangle2 = length2 * width2
+def Circle_Area(r): # Calculating area of circle
+    radius = r
+    area_circle1 = 3.14 * (radius ** 2)
+    print("Area of the circle is:", area_circle1)
 
-# Calculating area of circles
-radius1 = 4
-area_circle1 = 3.14 * (radius1 ** 2)
 
-radius2 = 6
-area_circle2 = 3.14 * (radius2 ** 2)
-
-print("Area of first rectangle:", area_rectangle1)
-print("Area of second rectangle:", area_rectangle2)
-print("Area of first circle:", area_circle1)
-print("Area of second circle:", area_circle2)
+# Execution 
+Rectangle_Area(10, 5)
+Rectangle_Area(15, 7)
+Circle_Area(4)
+Circle_Area(6)
