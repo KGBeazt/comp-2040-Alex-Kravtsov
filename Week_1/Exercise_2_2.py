dc29bfdb5dbd8080e4190c7e0dfd0d63044164c8
KGBeazt <alexkravtsov95@gmail.com>
2025-08-31 14:05:19 -0500

    add initial code for exercise 2.2

---
+++ Week_1/Exercise_2_2.py
@@ -0,0 +1,10 @@
+# Exercise 2.2
+# input
+data = "Name: Alice, Age: 30, City: Los Angeles | Name: Bob, Age: 25, City: Chicago | Name: Charlie , Age: 35, City: New York"
+
+# expected output
+[
+    {'name': 'Alice', 'age': '30', 'city': 'Los Angeles'},
+    {'name': 'Bob', 'age': '25', 'city': 'Chicago'},
+    {'name': 'Charlie', 'age': '35', 'city': 'New York'}
+]
