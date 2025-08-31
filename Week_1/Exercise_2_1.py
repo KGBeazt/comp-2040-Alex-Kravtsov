b143f9cc6a9c6f486bfe3fa8a5931528706dc0f0
KGBeazt <alexkravtsov95@gmail.com>
2025-08-31 13:56:21 -0500

    add initial code for exercise 2.1

---
+++ Week_1/Exercise_2_1.py
@@ -0,0 +1,10 @@
+# input string
+data = " Name: John Doe ; Age : 25;  CITY: New York ; Occupation: Software Engineer "
+
+# expected output
+{
+  'name': 'John Doe',
+  'age': '25',
+  'city': 'New York',
+  'occupation': 'Software Engineer'
+}
